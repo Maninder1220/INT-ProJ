044bddd490211ba2de7431982abb8c9371f9e8ac
Maninder1220 <maninderjitsinghjaura@gmail.com>
2024-05-16 13:15:44 +0530

    Starting with NAT

--- Terraform/Module/vpc/main.tf
+++ Terraform/Module/vpc/main.tf
@@ -32,5 +32,6 @@ resource "aws_subnet" "public_sn" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = var.public_sn_cidr
   availability_zone = data.aws_availability_zone.az
+  map_public_ip_on_launch = true
   
 }
